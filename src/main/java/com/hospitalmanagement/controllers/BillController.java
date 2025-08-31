8acd8af7d13ba93e72c0d47e7c5393ea5d8b601a
Durgesh Sharma <155382388+winter000boy@users.noreply.github.com>
2025-08-31 10:14:11 +0530

    Bill Api's Done

--- src/main/java/com/hospitalmanagement/controllers/BillController.java
+++ src/main/java/com/hospitalmanagement/controllers/BillController.java
@@ -4,44 +4,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hospitalmanagement.models.Bill;
+import com.hospitalmanagement.services.BillService;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/api/v1/bills")
 public class BillController {
 
+    @Autowired
+    private BillService billService;
+
     @GetMapping
     public List<Bill> getAllBills() {
-        return null;
+        return billService.getAllBills();
     }
 
     @PostMapping
-    public Bill registerBill(@RequestBody Bill bill){
-        return bill;
+    public Bill registerBill(@RequestBody Bill bill) {
+        return billService.createBill(bill);
     }
 
     @GetMapping("/{id}")
-    public Bill getBillById(@PathVariable Long id){
-        return null;
+    public Bill getBillById(@PathVariable Long id) {
+        return billService.getBillById(id);
     }
 
     @DeleteMapping("/id")
-    public void deleteBillById(@PathVariable Long id){}
+    public void deleteBillById(@PathVariable Long id) {
+        billService.deleteById(id);
+    }
 
     @PutMapping("/id")
-    public void updateBillById(@PathVariable Long id){}
-
-
-    
+    public Bill updateBillById(@PathVariable Long id, @RequestBody Bill bill) {
+        return billService.updateBill(id, bill);
+    }
 
 }
--- src/main/java/com/hospitalmanagement/services/BillService.java
+++ src/main/java/com/hospitalmanagement/services/BillService.java
@@ -1,5 +1,71 @@
 package com.hospitalmanagement.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hospitalmanagement.models.Bill;
+import com.hospitalmanagement.repository.BillRepository;
+
+@Service
 public class BillService {
+
+    @Autowired
+    private BillRepository billRepository
+
+    public List<Bill> getAllBills() {
+        try {
+            System.out.println("Into The Service Layer !");
+            return billRepository.findAll();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public Bill getBillById(Long id) {
+        try {
+            Optional<Bill> bill = billRepository.findById(id);
+            return bill.orElse(null);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public Bill createBill(Bill bill) {
+        try {
+            return billRepository.save(bill);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public void deleteById(Long id) {
+        try {
+            billRepository.deleteById(id);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Bill updateBill(Long id, Bill existingBill) {
+        try {
+            Optional<Bill> updatedBill = billRepository.findById(id);
+            if (updatedBill.isPresent()) {
+                Bill bill = updatedBill.get();
+                bill.setBillAmount(existingBill.getBillAmount());
+                bill.setStatus(existingBill.getStatus());
+                return billRepository.save(bill);
+            }
+            return null;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 
 }
